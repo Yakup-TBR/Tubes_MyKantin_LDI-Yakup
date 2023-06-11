0d472aed073c5af03717c798ddcd79d7bc6aa85d
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-06-11 16:51:42 +0800

    Update login
    tambah coba3

--- MyKantin/Login.cs
+++ MyKantin/Login.cs
@@ -19,6 +19,8 @@ namespace MyKantin
 
         private String coba = "ripo1";
         private String coba2 = "hapid";
+        private String coba3 = "hapid1";
+
 
         private void Form1_Load(object sender, EventArgs e) 
         {
