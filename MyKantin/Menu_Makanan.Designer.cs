ab942c09eb332b5c7259cbb8b5dddc3c9c56a3f5
rifqi10 <den.rifqihafid@gmail.com>
2023-06-11 22:50:53 +0700

    Update DB, login, menu makanan/minuman

--- MyKantin/Menu_Makanan.Designer.cs
+++ MyKantin/Menu_Makanan.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace MyKantin
+﻿using System;
+
+namespace MyKantin
 {
     partial class Menu_Makanan
     {
@@ -42,6 +44,7 @@
             this.pictureBox8 = new System.Windows.Forms.PictureBox();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
@@ -52,6 +55,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelPilihPembayaran
@@ -59,9 +63,10 @@
             this.labelPilihPembayaran.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.labelPilihPembayaran.AutoSize = true;
             this.labelPilihPembayaran.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelPilihPembayaran.Location = new System.Drawing.Point(125, 26);
+            this.labelPilihPembayaran.Location = new System.Drawing.Point(94, 21);
+            this.labelPilihPembayaran.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelPilihPembayaran.Name = "labelPilihPembayaran";
-            this.labelPilihPembayaran.Size = new System.Drawing.Size(248, 25);
+            this.labelPilihPembayaran.Size = new System.Drawing.Size(198, 21);
             this.labelPilihPembayaran.TabIndex = 11;
             this.labelPilihPembayaran.Text = "Menu Kantin Kedai Yummy";
             // 
@@ -69,17 +74,20 @@
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(36, 90);
+            this.textBox1.Location = new System.Drawing.Point(27, 73);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(360, 20);
+            this.textBox1.Size = new System.Drawing.Size(270, 16);
             this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChange);
             // 
             // pictureBox10
             // 
             this.pictureBox10.Image = global::MyKantin.Properties.Resources.Siomay;
-            this.pictureBox10.Location = new System.Drawing.Point(247, 533);
+            this.pictureBox10.Location = new System.Drawing.Point(164, 268);
+            this.pictureBox10.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(210, 159);
+            this.pictureBox10.Size = new System.Drawing.Size(158, 129);
             this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox10.TabIndex = 21;
             this.pictureBox10.TabStop = false;
@@ -87,39 +95,46 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::MyKantin.Properties.Resources.Rendang;
-            this.pictureBox5.Location = new System.Drawing.Point(247, 368);
+            this.pictureBox5.Location = new System.Drawing.Point(2, 268);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(210, 159);
+            this.pictureBox5.Size = new System.Drawing.Size(158, 129);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 20;
             this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
             // 
             // pictureBox4
             // 
             this.pictureBox4.Image = global::MyKantin.Properties.Resources.Gado2;
-            this.pictureBox4.Location = new System.Drawing.Point(24, 533);
+            this.pictureBox4.Location = new System.Drawing.Point(2, 135);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(210, 159);
+            this.pictureBox4.Size = new System.Drawing.Size(158, 129);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 19;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             // 
             // pictureBox3
             // 
             this.pictureBox3.Image = global::MyKantin.Properties.Resources.Soto;
-            this.pictureBox3.Location = new System.Drawing.Point(24, 368);
+            this.pictureBox3.Location = new System.Drawing.Point(164, 135);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(210, 159);
+            this.pictureBox3.Size = new System.Drawing.Size(158, 129);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 18;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::MyKantin.Properties.Resources.Nasi_Gor;
-            this.pictureBox2.Location = new System.Drawing.Point(247, 203);
+            this.pictureBox2.Location = new System.Drawing.Point(164, 2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(210, 159);
+            this.pictureBox2.Size = new System.Drawing.Size(158, 129);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 17;
             this.pictureBox2.TabStop = false;
@@ -128,39 +143,46 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::MyKantin.Properties.Resources.Bakso;
-            this.pictureBox1.Location = new System.Drawing.Point(24, 203);
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(210, 159);
+            this.pictureBox1.Size = new System.Drawing.Size(158, 129);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // pictureBox6
             // 
             this.pictureBox6.Image = global::MyKantin.Properties.Resources.Makanan_putih;
-            this.pictureBox6.Location = new System.Drawing.Point(130, 139);
+            this.pictureBox6.Location = new System.Drawing.Point(98, 113);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(216, 49);
+            this.pictureBox6.Size = new System.Drawing.Size(162, 40);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox6.TabIndex = 15;
             this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
             // 
             // pictureBox9
             // 
             this.pictureBox9.Image = global::MyKantin.Properties.Resources.ikon_search;
-            this.pictureBox9.Location = new System.Drawing.Point(416, 88);
+            this.pictureBox9.Location = new System.Drawing.Point(312, 72);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(28, 27);
+            this.pictureBox9.Size = new System.Drawing.Size(21, 22);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox9.TabIndex = 14;
             this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
             // 
             // pictureBox8
             // 
             this.pictureBox8.Image = global::MyKantin.Properties.Resources.search_bar;
-            this.pictureBox8.Location = new System.Drawing.Point(24, 81);
+            this.pictureBox8.Location = new System.Drawing.Point(18, 66);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(429, 42);
+            this.pictureBox8.Size = new System.Drawing.Size(322, 34);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox8.TabIndex = 12;
             this.pictureBox8.TabStop = false;
@@ -168,9 +190,10 @@
             // pictureBox7
             // 
             this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
-            this.pictureBox7.Location = new System.Drawing.Point(36, 24);
+            this.pictureBox7.Location = new System.Drawing.Point(27, 20);
+            this.pictureBox7.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(34, 27);
+            this.pictureBox7.Size = new System.Drawing.Size(26, 22);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox7.TabIndex = 9;
             this.pictureBox7.TabStop = false;
@@ -180,33 +203,43 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(253, 154);
+            this.label1.Location = new System.Drawing.Point(190, 125);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 19);
+            this.label1.Size = new System.Drawing.Size(59, 16);
             this.label1.TabIndex = 36;
             this.label1.Text = "Minuman";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox2);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox4);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox3);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox5);
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox10);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 158);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(338, 409);
+            this.flowLayoutPanel1.TabIndex = 37;
+            // 
             // Menu_Makanan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(482, 713);
+            this.ClientSize = new System.Drawing.Size(362, 579);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox10);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox9);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox8);
             this.Controls.Add(this.labelPilihPembayaran);
             this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Menu_Makanan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu_Makanan";
@@ -220,10 +253,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
+
+   
 
         #endregion
 
@@ -240,5 +276,6 @@
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox10;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- MyKantin/Menu_Minuman.cs
+++ MyKantin/Menu_Minuman.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +8,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
 
 namespace MyKantin
 {
     public partial class Menu_Minuman : Form
     {
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+        MySqlCommand command;
+        MySqlDataAdapter da;
+
         public Menu_Minuman()
         {
             InitializeComponent();
@@ -45,6 +52,127 @@ namespace MyKantin
         private void pictureBox6_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            string searchText = textBox1.Text.ToLower();
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox10.Visible = false;
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                string connectionString = "Server=127.0.0.1;Database=mykantin;Uid=root";
+                MySqlConnection connection = new MySqlConnection(connectionString);
+
+                try
+                {
+                    connection.Open();
+
+                    string query = "SELECT * FROM produk_tbl WHERE nama_produk LIKE @keyword";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@keyword", "%" + searchText + "%");
+
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    bool MenuDitemukan = false; // Menandakan apakah kantin ditemukan atau tidak
+
+                    while (reader.Read())
+                    {
+                        string namaProduk = reader.GetString("nama_produk");
+
+                        if (namaProduk.Equals("Air Mineral", StringComparison.OrdinalIgnoreCase))
+                        {
+                            pictureBox1.Visible = true;
+                            MenuDitemukan = true;
+                        }
+                        else if (namaProduk.Equals("Es Teh", StringComparison.OrdinalIgnoreCase))
+                        {
+                            pictureBox2.Visible = true;
+                            MenuDitemukan = true;
+                        }
+                        else if (namaProduk.Equals("Es Buah Pelangi ", StringComparison.OrdinalIgnoreCase))
+                        {
+                            pictureBox3.Visible = true;
+                            MenuDitemukan = true;
+                        }
+                        else if (namaProduk.Equals("Es Cincau Milo", StringComparison.OrdinalIgnoreCase))
+                        {
+                            pictureBox4.Visible = true;
+                            MenuDitemukan = true;
+                        }
+                        else if (namaProduk.Equals("Choco Es Milo", StringComparison.OrdinalIgnoreCase))
+                        {
+                            pictureBox5.Visible = true;
+                            MenuDitemukan = true;
+                        }
+                        else if (namaProduk.Equals("Brown Sugar Milk", StringComparison.OrdinalIgnoreCase))
+                        {
+                            pictureBox10.Visible = true;
+                            MenuDitemukan = true;
+                        }
+                    }
+
+                    if (!MenuDitemukan)
+                    {
+                        MessageBox.Show("Menu Minuman tidak ditemukan.");
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+            else
+            {
+                pictureBox1.Visible = true;
+                pictureBox2.Visible = true;
+                pictureBox3.Visible = true;
+                pictureBox4.Visible = true;
+                pictureBox5.Visible = true;
+                pictureBox10.Visible = true;
+            }
         }
     }
 }
